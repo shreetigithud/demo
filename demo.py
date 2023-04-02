7cd1b8eedb6636757147f9d6ec8ae1580f46de58
shreeti saha <118799210+shreetigithud@users.noreply.github.com>
2023-04-02 15:51:50 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+#shreetisaha
+print('hello')
+
+
+# In[4]:
+
+
+a=int(input("enter the value"))
+
+
+# In[5]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[ ]:
+
+
+
+
